f7ab980752ea15977ef688d91fedeb23a4d3d34b
Alina Gorodetskaya <gorodetskaya_alina1998@mail.ru>
2022-04-27 22:23:10 +0300

    fix: после ревью

--- script/script.js
+++ script/script.js
@@ -2,47 +2,36 @@ const openPopupButton = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__close');
 
-function popupOpenToggle() {
-    popup.classList.toggle('popup_opened');
-}
-
-function popupOverlayClickHandler(evt) {
-    if (evt.target === evt.currentTarget) {
-        popupOpenToggle();
-    }
-}
-
-openPopupButton.addEventListener('click', popupOpenToggle);
-closePopupButton.addEventListener('click', popupOpenToggle);
-popup.addEventListener('click', popupOverlayClickHandler);
-
-
-
 let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__activity'); 
-let profileNameValue = profileName.textContent;
-let profileJobValue = profileJob.textContent;
+let profileJob = document.querySelector('.profile__activity');
+let formElement = popup.querySelector('.popup__form');
+let popupName = popup.querySelector('.popup__form-item_user-info_name');
+let popupJob = popup.querySelector('.popup__form-item_user-info_job');
 
-let inputs = document.querySelectorAll('input');
+//открытие попапа
+function popupOpen() {
+    popupName.value = profileName.textContent;
+    popupJob.value = profileJob.textContent;
+    popup.classList.add('popup_opened');
+}
 
-inputs[0].value = profileNameValue;
-inputs[1].value = profileJobValue;
+//закрытие попапа
+function popupClose() {
+    popup.classList.remove('popup_opened');
+}
 
+openPopupButton.addEventListener('click', popupOpen);
+closePopupButton.addEventListener('click', popupClose);
 
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__form-item_user-info_name');
-let jobInput = document.querySelector('.popup__form-item_user-info_job');
-
+//функция добавляет данные, введенные пользователем, в профиль
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    let nameInputValue = nameInput.value;
-    let jobInputValue = jobInput.value;
+    profileName.textContent = popupName.value;
+    profileJob.textContent = popupJob.value;
 
-    profileName.textContent = nameInputValue;
-    profileJob.textContent = jobInputValue;
+    popupClose();
 }
 
+//кнопка сохранения профиля
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', popupOverlayClickHandler);
